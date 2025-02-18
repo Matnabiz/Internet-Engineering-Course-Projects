3671d65cd3349a55579b2102375ca7eac763a79c
Matin Nabizadeh <matnabiz@yahoo.com>
2025-02-18 23:58:41 +0330

    fix: Changed the initial log message.

--- CA1/src/main/java/org/example/Main.java
+++ CA1/src/main/java/org/example/Main.java
@@ -4,7 +4,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.printf("Hello and welcome!\n");
+        System.out.printf("Hello and welcome to the hotel management system!\n");
         Hotel hotel = new Hotel();
 
         System.out.println("Customers: " + hotel.getAllCustomers().size());
