c3b545541693deafcdbeb3a456aae8cf90da4248
Matin Nabizadeh <matnabiz@yahoo.com>
2025-02-19 13:27:24 +0330

    fix: Fixed the naming system of the class BookingsInRoom to the convention used for the whole project.

--- CA1/src/main/java/org/example/BookingsInRoom.java
+++ CA1/src/main/java/org/example/BookingsInRoom.java
@@ -3,19 +3,18 @@ package org.example;
 import java.util.List;
 
 public class BookingsInRoom {
-    private int roomId;
+    private int roomNumber;
     private int capacity;
     private List<BookingWithCustomer> bookings;
 
     public BookingsInRoom(int roomId, int capacity, List<BookingWithCustomer> bookings) {
-        this.roomId = roomId;
+        this.roomNumber = roomNumber;
         this.capacity = capacity;
         this.bookings = bookings;
     }
 
-    // Getters and Setters
-    public int getRoomId() { return roomId; }
-    public void setRoomId(int roomId) { this.roomId = roomId; }
+    public int getRoomNumber() { return this.roomNumber; }
+    public void setRoomNumber(int roomNumber) { this.roomNumber = roomNumber; }
 
     public int getCapacity() { return capacity; }
     public void setCapacity(int capacity) { this.capacity = capacity; }
