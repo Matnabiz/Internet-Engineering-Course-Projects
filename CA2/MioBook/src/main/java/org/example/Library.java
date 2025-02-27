d30f30da9d654bc521a66fe73777da6989f40314
Matin Nabizadeh <matnabiz@yahoo.com>
2025-02-27 23:29:01 +0330

    fix: Fixed some bugs in deleteBookFromCart method.

--- CA2/MioBook/src/main/java/org/example/Library.java
+++ CA2/MioBook/src/main/java/org/example/Library.java
@@ -12,11 +12,6 @@ public class Library {
     private String success;
     private  String message;
 
-    public void addUser(String role, String username, String password, String email, Address address){
-
-
-    }
-
     private boolean userExists(String username){
         return users.stream().anyMatch(user -> user.getUsername().equals(username));
     }
@@ -44,6 +39,7 @@ public class Library {
     private boolean validateRole(String role){
         return role.equalsIgnoreCase("customer") && !role.equalsIgnoreCase("admin");
     }
+
     public void addUser(String username, String password, String email, String address, String role) {
 
         if (!validateUsername(username)) {
@@ -89,8 +85,9 @@ public class Library {
         message = "User successfully registered!";
         success = "true";
     }
-    public void addBookToCart (String userName, String bookTitle) {
-        if(!userExists(userName)){
+
+    public void addBookToCart (String username, String bookTitle) {
+        if(!userExists(username)){
             message = "User doesn't exist!";
             success = "false" ;
             return;
@@ -102,7 +99,7 @@ public class Library {
         }
 
         User Customer = users.stream()
-                .filter(u -> u.getUsername().equals(userName))
+                .filter(u -> u.getUsername().equals(username))
                 .findFirst()
                 .orElse(null);
 
@@ -127,32 +124,36 @@ public class Library {
         }
     }
 
-    public void deleteBookFromCart(String userName, String bookTitle){
-        boolean existsUser = users.stream().anyMatch(user -> user.getUsername().equals(userName));
-        boolean existsBook = books.stream().anyMatch(book -> book.getTitle().equals(bookTitle));
-        if(!existsUser){
+    public void deleteBookFromCart(String username, String bookTitle){
+
+        if(!userExists(username)){
             message = "User doesn't exist!";
             success = "false" ;
+            return;
         }
-        if(!existsBook){
+        if(!bookExists(bookTitle)){
             message = "Book doesn't exist!";
             success = "false" ;
+            return;
         }
 
-        Optional<User> Buyer = users.stream().filter(u -> u.getUsername().equals(userName)).findFirst();
-        Optional<Book> B_Book = books.stream().filter(b -> b.getTitle().equals(bookTitle)).findFirst();
+        User customer = users.stream().filter(u -> u.getUsername().equals(username)).findFirst().orElse(null);
+        Book bookInCart = books.stream().filter(b -> b.getTitle().equals(bookTitle)).findFirst().orElse(null);
+        if (customer == null || bookInCart == null) return;
 
-        if(Buyer.getRole().equals("admin")) {
-            message = "You are admin , you can't do this !";
+        if(customer.getRole().equals("admin")) {
+            message = "You are admin , you can't do this!";
             success = "false" ;
         }
-        else if(!Buyer.getShppingCart().contains(B_Book)){
-            message = "You don't have this book in your cart !";
+
+        else if(!customer.getShoppingCart().contains(bookInCart)){
+            message = "You don't have this book in your cart!";
             success = "false" ;
         }
-        else {
-            int i = users.indexOf(Buyer);
-            users.get(i).deleteShppingBook(B_Book);
-        }
+
+
+        customer.deleteShoppingBook(bookInCart);
+        message = "Book removed from cart successfully!";
+        success = "true";
     }
 }
