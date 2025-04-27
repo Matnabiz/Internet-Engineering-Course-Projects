de97bed505e85704933113307511803a4027e6ae
Matin Nabizadeh <matnabiz@yahoo.com>
2025-04-27 10:24:17 +0330

    feat: Added lombok to the entities.

--- CA5/demo/src/main/java/com/example/library/entity/AuthorEntity.java
+++ CA5/demo/src/main/java/com/example/library/entity/AuthorEntity.java
@@ -1,10 +1,12 @@
 package com.example.library.entity;
-
+import lombok.*;
 import jakarta.persistence.*;
 import java.time.LocalDate;
 
 @Entity
 @Table(name = "authors")
+@Getter
+@Setter
 public class AuthorEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -15,60 +17,5 @@ public class AuthorEntity {
     private String penName;
     private String nationality;
     private LocalDate born;
-    private LocalDate died; // Optional
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setPenName(String penName) {
-        this.penName = penName;
-    }
-
-    public void setNationality(String nationality) {
-        this.nationality = nationality;
-    }
-
-    public void setBorn(LocalDate born) {
-        this.born = born;
-    }
-
-    public void setDied(LocalDate died) {
-        this.died = died;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getPenName() {
-        return penName;
-    }
-
-    public String getNationality() {
-        return nationality;
-    }
-
-    public LocalDate getBorn() {
-        return born;
-    }
-
-    public LocalDate getDied() {
-        return died;
-    }
+    private LocalDate died;
 }
--- CA5/demo/src/main/java/com/example/library/entity/UserEntity.java
+++ CA5/demo/src/main/java/com/example/library/entity/UserEntity.java
@@ -1,9 +1,12 @@
 package com.example.library.entity;
-
+import lombok.*;
 import jakarta.persistence.*;
+import lombok.Getter;
 
 @Entity
 @Table(name = "users")
+@Getter
+@Setter
 public class UserEntity {
     @Id
     private String username;
@@ -16,21 +19,4 @@ public class UserEntity {
     @Embedded
     private AddressEmbeddable address;
 
-    public String getUsername() { return username; }
-    public void setUsername(String username) { this.username = username; }
-
-    public String getPassword() { return password; }
-    public void setPassword(String password) { this.password = password; }
-
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
-
-    public String getRole() { return role; }
-    public void setRole(String role) { this.role = role; }
-
-    public int getBalance() { return balance; }
-    public void setBalance(int balance) { this.balance = balance; }
-
-    public AddressEmbeddable getAddress() { return address; }
-    public void setAddress(AddressEmbeddable address) { this.address = address; }
 }
